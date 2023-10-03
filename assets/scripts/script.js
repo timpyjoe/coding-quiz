47469625363beecfbb87a1f7f92ea019c191cb95
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-02 23:02:53 -0500

    Adds countdown timer in upper right corner

---
+++ assets/scripts/script.js
@@ -0,0 +1,17 @@
+var timeLeft = 10;
+var timerEl = document.querySelector("#timer");
+
+
+function setTimer() {
+  var timeInterval = setInterval(function(){
+    timerEl.textContent =  `${timeLeft} second(s) remaining`;
+    timeLeft--;
+
+    if (timeLeft < 0) {
+      clearInterval(timeInterval);
+      timerEl.textContent = "";
+    }
+  }, 1000);
+}
+
+setTimer();
