77d8d971895f5490eaaf09af653bd3554b75e001
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-10-04 21:50:39 -0500

    webpage now populates quiz questions upon game start and correct question answer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,20 @@
 var timeLeft = 10;
 var startScreen = document.querySelector(".start-screen");
+var score = 0;
+var index = 0;
 
 var timerEl = document.querySelector("#timer");
 var quiz = document.querySelector(".question-block");
+// startScreen.setAttribute("style", "visibility:visible");
+// quiz.setAttribute("style", "visibility:hidden");
+
+var question = document.querySelector("#question");
 var button1 = document.querySelector("#choice1");
 var button2 = document.querySelector("#choice2");
 var button3 = document.querySelector("#choice3");
 var button4 = document.querySelector("#choice4");
 
+// Actual quiz questions
 var question1 = {
   question: ".addEventListener is an example of which of the following?",
   choice1: "Function",
@@ -29,13 +36,40 @@ var question2 = {
 var questions = [question1, question2];
 
 var startBtn = document.querySelector("#startBtn");
-startBtn.addEventListener("click", function(event) {
-  setTimer();
-  startScreen.setAttribute("style", "visibility:hidden");
-  quiz.setAttribute("style", "visibility:visible");
+startBtn.addEventListener("click", startGame);
 
+// Gets a random index from an array
+function getRandom(array) {
+  index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
-)
+
+// displays the question on the screen (with its choices)
+function setQuestion() {
+  var currentQuestion = getRandom(questions);
+  console.log(currentQuestion);
+  console.log(questions);
+  question.textContent = currentQuestion.question;
+  button1.textContent = currentQuestion.choice1;
+  button2.textContent = currentQuestion.choice2;
+  button3.textContent = currentQuestion.choice3;
+  button4.textContent = currentQuestion.choice4;
+  quiz.addEventListener("click", function(event) {
+    var clicked = event.target;
+    if (clicked.getAttribute("id") === currentQuestion.answer) {
+      score += 5;
+      questions.splice(index, 1);
+      console.log(questions);
+      if (questions.length > 0) {
+      setQuestion();
+      }
+    }
+  })
+}
+
+function endGame() {
+  
+}
 
 
 
@@ -51,6 +85,10 @@ function setTimer() {
   }, 1000);
 }
 
+function startGame() {
+  setTimer();
+  startScreen.setAttribute("style", "visibility:hidden");
+  quiz.setAttribute("style", "visibility:visible");
+  setQuestion();
 
-console.log(question1.choice4);
-// setTimer();
+}
